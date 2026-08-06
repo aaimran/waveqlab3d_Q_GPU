b29f6701c2caf6539f880184372b821885e550e1
Imran <aimran@punakha.utep.edu>
2026-08-06 17:53:13 -0600

    Map RK unit arrays with OpenACC directives

--- tests/rk_vector_test_data_openacc.f90
+++ tests/rk_vector_test_data_openacc.f90
@@ -5,18 +5,10 @@ module rk_vector_test_data
 contains
   subroutine enter_test_data(state, rate)
     real(kind=wp), allocatable, intent(inout) :: state(:,:,:,:), rate(:,:,:,:)
-    integer(kind=8) :: state_bytes, rate_bytes
-    state_bytes=int(size(state),8)*int(storage_size(0.0_wp)/8,8)
-    rate_bytes=int(size(rate),8)*int(storage_size(0.0_wp)/8,8)
-    call acc_copyin(state,state_bytes)
-    call acc_copyin(rate,rate_bytes)
+    !$acc enter data copyin(state,rate)
   end subroutine
   subroutine exit_test_data(state, rate)
     real(kind=wp), allocatable, intent(inout) :: state(:,:,:,:), rate(:,:,:,:)
-    integer(kind=8) :: state_bytes, rate_bytes
-    state_bytes=int(size(state),8)*int(storage_size(0.0_wp)/8,8)
-    rate_bytes=int(size(rate),8)*int(storage_size(0.0_wp)/8,8)
-    call acc_delete_finalize(rate,rate_bytes)
-    call acc_delete_finalize(state,state_bytes)
+    !$acc exit data delete(rate,state) finalize
   end subroutine
 end module rk_vector_test_data
